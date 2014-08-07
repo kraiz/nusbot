f08efc6ebc497c94d9605621ea582e5e7db7b5bb
kraiz <lars.kreisz@gmail.com>
2014-08-07 22:15:35 +0200

    removed version import

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup, find_packages
 
-import nusbot
-
 
 setup(
     name='nusbot',
-    version='.'.join(str(e) for e in nusbot.__version__),
+    version='0.1.2',
     description='adc bot annoucing changes in filelist of hub\'s users',
     url='https://github.com/kraiz/nusbot',
     license='MIT',
