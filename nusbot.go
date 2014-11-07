9759729e3bf049808f8a57ed34519f8f9a9f0f1d
kraiz <lars.kreisz@gmail.com>
2014-11-07 22:52:23 +0100

    added regexes for pasring commands

--- nusbot.go
+++ nusbot.go
@@ -34,18 +34,23 @@ const (
 
 const (
 	STA = "STA"
+	SUP = "SUP"
+	SID = "SID"
 )
 
 const (
 	PatternMsgType = "[" + MsgBroadcast + MsgClient + MsgDirect + MsgEcho + MsgFeature + MsgHub + MsgInfo + MsgUdp + "]"
-	PatternCommand = STA
-	PatternSTA     = `^(?P<severity>\d)(?P<error_code>\d{2})\s(?P<text>.*)$`
+	PatternCommand = STA + "|" + SUP + "|" + SID
 )
 
-var patternMsg = fmt.Sprintf(`^(%s)(%s)\s(.*)`, PatternMsgType, PatternCommand)
-var reMsg = regexp.MustCompile(patternMsg)
-var reSTA = regexp.MustCompile(PatternSTA)
+var reMsg = regexp.MustCompile(`^(?P<msgType>` + PatternMsgType + `)(?P<command>` + PatternCommand + `)\s(?P<args>.*)`)
+var reArgsMap = map[string]*regexp.Regexp{
+	STA: regexp.MustCompile(`^(?P<severity>\d)(?P<errorCode>\d{2})\s(?P<description>.*)$`),
+	SUP: regexp.MustCompile(`(AD|RM)(\w*)`),
+	SID: regexp.MustCompile(`^(?P<sid>\w{4})$`),
+}
 
+// helpers
 func escape(text string) string {
 	return strings.Replace(text, " ", `\s`, -1)
 }
@@ -54,11 +59,30 @@ func unescape(text string) string {
 	return strings.Replace(text, `\s`, " ", -1)
 }
 
+func parse(re *regexp.Regexp, str string) map[string]string {
+	submatchMap := make(map[string]string)
+	submatches := re.FindStringSubmatch(str)
+
+	if submatches == nil {
+		return nil
+	}
+
+	for index, name := range re.SubexpNames() {
+		if index == 0 || name == "" {
+			continue
+		}
+		submatchMap[name] = submatches[index]
+	}
+
+	return submatchMap
+}
+
 type Bot struct {
 	server string
 	nick   string
 	conn   net.Conn
 	state  HubState
+	sid    string
 }
 
 func (bot *Bot) Run() {
@@ -77,27 +101,36 @@ func (bot *Bot) Run() {
 		if err != nil {
 			log.Fatal("Can not read from connection", err)
 		}
-		match := reMsg.FindStringSubmatch(line)
-		if len(match) != 4 {
+
+		if match := parse(reMsg, line); match != nil {
+			bot.Handle(match["msgType"], match["command"], parse(reArgsMap[match["command"]], match["args"]))
+		} else {
 			log.Println("Ignoring unknown command:", line)
-			continue
 		}
-		bot.Handle(match[1], match[2], match[3])
 	}
 }
 
-func (bot *Bot) Handle(msgType string, cmd string, args string) {
-	fmt.Println(msgType, cmd, args)
-
+func (bot *Bot) Handle(msgType string, cmd string, args map[string]string) {
 	switch cmd {
 	case STA:
-		match := reSTA.FindStringSubmatch(args)
-		fmt.Println("jaaaaa", unescape(match[3]))
+		if args["severity"] == "0" {
+			log.Println("INFO:", unescape(args["description"]))
+		} else {
+			log.Fatalf("Error %s received from hub: %s", args["errorCode"], unescape(args["description"]))
+		}
+	case SUP:
+		if bot.state == INITIAL {
+			bot.state = PROTOCOL
+		}
+	case SID:
+		bot.sid = args["sid"]
+		log.Println("Got Session ID:", bot.sid)
+	default:
+		fmt.Println(msgType, cmd, args)
 	}
 }
 
 func main() {
-	fmt.Println(patternMsg)
 	bot := &Bot{server: "10.10.0.1:1511", nick: "nusbot2"}
 	bot.Run()
 }
