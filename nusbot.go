bca5ba202fc38871be76a8830b4dc5ad34e60c23
kraiz <lars.kreisz@gmail.com>
2014-11-08 21:52:03 +0100

    added key value parsing of different command arguments

--- nusbot.go
+++ nusbot.go
@@ -36,18 +36,35 @@ const (
 	STA = "STA"
 	SUP = "SUP"
 	SID = "SID"
+	INF = "INF"
 )
 
 const (
 	PatternMsgType = "[" + MsgBroadcast + MsgClient + MsgDirect + MsgEcho + MsgFeature + MsgHub + MsgInfo + MsgUdp + "]"
-	PatternCommand = STA + "|" + SUP + "|" + SID
+	PatternCommand = STA + "|" + SUP + "|" + SID + "|" + INF
 )
 
 var reMsg = regexp.MustCompile(`^(?P<msgType>` + PatternMsgType + `)(?P<command>` + PatternCommand + `)\s(?P<args>.*)`)
 var reArgsMap = map[string]*regexp.Regexp{
 	STA: regexp.MustCompile(`^(?P<severity>\d)(?P<errorCode>\d{2})\s(?P<description>.*)$`),
-	SUP: regexp.MustCompile(`(AD|RM)(\w*)`),
+	SUP: regexp.MustCompile(`^(?P<keyvalues>.*)$`),
 	SID: regexp.MustCompile(`^(?P<sid>\w{4})$`),
+	INF: regexp.MustCompile(`^(?P<keyvalues>.*)$`),
+}
+
+var reKeyValueMap = map[string]*regexp.Regexp{
+	SUP: regexp.MustCompile(`(?P<value>AD|RM)(?P<key>\w*)`),
+	INF: regexp.MustCompile(`(?P<key>\w{2})(?P<value>[\w\\s]*)`),
+}
+
+var inf = map[string]string{
+	"ID": "SXX4RUEEB263P3EX7VAGSMHGO4XVDBTQOJZNONI",
+	"PD": "4MH2IBPDTOP34ELXWSXRY35CSTHDR3PCOMWZPMI",
+	"CT": "1",
+	"NI": "nusbot",
+	"VE": escape("nusbot 0.2.0"),
+	"DE": escape("I'm a bot, type $help into the chat"),
+	"SU": "TCP4",
 }
 
 // helpers
@@ -59,7 +76,7 @@ func unescape(text string) string {
 	return strings.Replace(text, `\s`, " ", -1)
 }
 
-func parse(re *regexp.Regexp, str string) map[string]string {
+func parseArgs(re *regexp.Regexp, str string) map[string]string {
 	submatchMap := make(map[string]string)
 	submatches := re.FindStringSubmatch(str)
 
@@ -75,6 +92,26 @@ func parse(re *regexp.Regexp, str string) map[string]string {
 	}
 
 	return submatchMap
+}
+
+func parseKeyValues(re *regexp.Regexp, str string) map[string]string {
+	keyvalues := make(map[string]string)
+
+	if re == nil || str == "" {
+		return keyvalues
+	}
+
+	submatches := re.FindAllStringSubmatch(str, -1)
+	keyFirst := re.SubexpNames()[1] == "key"
+
+	for _, submatch := range submatches {
+		if keyFirst {
+			keyvalues[submatch[1]] = submatch[2]
+		} else {
+			keyvalues[submatch[2]] = submatch[1]
+		}
+	}
+	return keyvalues
 }
 
 type AdcConnection struct {
@@ -102,15 +139,18 @@ func (ac *AdcConnection) Run() {
 			log.Fatal("Can not read from connection", err)
 		}
 
-		if match := parse(reMsg, line); match != nil {
-			ac.Handle(match["msgType"], match["command"], parse(reArgsMap[match["command"]], match["args"]))
+		if match := parseArgs(reMsg, line); match != nil {
+			args := parseArgs(reArgsMap[match["command"]], match["args"])
+			keyvalues := parseKeyValues(reKeyValueMap[match["command"]], args["keyvalues"])
+			delete(args, "keyvalues")
+			ac.Handle(match["msgType"], match["command"], args, keyvalues)
 		} else {
 			log.Println("Ignoring unknown command:", line)
 		}
 	}
 }
 
-func (ac *AdcConnection) Handle(msgType string, cmd string, args map[string]string) {
+func (ac *AdcConnection) Handle(msgType string, cmd string, args map[string]string, keyvalues map[string]string) {
 	switch cmd {
 	case STA:
 		if args["severity"] == "0" {
@@ -126,7 +166,7 @@ func (ac *AdcConnection) Handle(msgType string, cmd string, args map[string]stri
 		ac.sid = args["sid"]
 		log.Println("Got Session ID:", ac.sid)
 	default:
-		fmt.Println(msgType, cmd, args)
+		fmt.Println(msgType, cmd, args, keyvalues)
 	}
 }
 
