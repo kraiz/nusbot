5316aa279867eedcbd1550d824439167b6ddcd32
kraiz <lars.kreisz@gmail.com>
2014-11-08 22:15:50 +0100

    restructured global variables and added some doc lines

--- nusbot.go
+++ nusbot.go
@@ -10,8 +10,10 @@ import (
 	"strings"
 )
 
+// Type for differentiate states if ADC connection.
 type State int
 
+// Different value of an ADC connection.
 const (
 	INITIAL State = iota
 	PROTOCOL
@@ -21,6 +23,7 @@ const (
 	DATA
 )
 
+// Message types
 const (
 	MsgBroadcast = "B"
 	MsgClient    = "C"
@@ -32,6 +35,7 @@ const (
 	MsgUdp       = "U"
 )
 
+// Commands that we handle
 const (
 	STA = "STA"
 	SUP = "SUP"
@@ -39,43 +43,51 @@ const (
 	INF = "INF"
 )
 
+// Intermediate regex items for building the complex reMsg
 const (
 	PatternMsgType = "[" + MsgBroadcast + MsgClient + MsgDirect + MsgEcho + MsgFeature + MsgHub + MsgInfo + MsgUdp + "]"
 	PatternCommand = STA + "|" + SUP + "|" + SID + "|" + INF
 )
 
-var reMsg = regexp.MustCompile(`^(?P<msgType>` + PatternMsgType + `)(?P<command>` + PatternCommand + `)\s(?P<args>.*)`)
-var reArgsMap = map[string]*regexp.Regexp{
-	STA: regexp.MustCompile(`^(?P<severity>\d)(?P<errorCode>\d{2})\s(?P<description>.*)$`),
-	SUP: regexp.MustCompile(`^(?P<keyvalues>.*)$`),
-	SID: regexp.MustCompile(`^(?P<sid>\w{4})$`),
-	INF: regexp.MustCompile(`^(?P<keyvalues>.*)$`),
-}
+var (
+	// Overvall regex to parse each incoming command.
+	reMsg = regexp.MustCompile(`^(?P<msgType>` + PatternMsgType + `)(?P<command>` + PatternCommand + `)\s(?P<args>.*)`)
+	// Regex to parse the arguments per command.
+	reArgsMap = map[string]*regexp.Regexp{
+		STA: regexp.MustCompile(`^(?P<severity>\d)(?P<errorCode>\d{2})\s(?P<description>.*)$`),
+		SUP: regexp.MustCompile(`^(?P<keyvalues>.*)$`),
+		SID: regexp.MustCompile(`^(?P<sid>\w{4})$`),
+		INF: regexp.MustCompile(`^(?P<keyvalues>.*)$`),
+	}
+	// Some commands contain key value pairs. Thats the regex to parse them.
+	reKeyValueMap = map[string]*regexp.Regexp{
+		SUP: regexp.MustCompile(`(?P<value>AD|RM)(?P<key>\w*)`),
+		INF: regexp.MustCompile(`(?P<key>\w{2})(?P<value>[\w\\s]*)`),
+	}
+	// Information data send to hub and other clients
+	inf = map[string]string{
+		"ID": "SXX4RUEEB263P3EX7VAGSMHGO4XVDBTQOJZNONI",
+		"PD": "4MH2IBPDTOP34ELXWSXRY35CSTHDR3PCOMWZPMI",
+		"CT": "1",
+		"NI": "nusbot",
+		"VE": escape("nusbot 0.2.0"),
+		"DE": escape("I'm a bot, type $help into the chat"),
+		"SU": "TCP4",
+	}
+)
 
-var reKeyValueMap = map[string]*regexp.Regexp{
-	SUP: regexp.MustCompile(`(?P<value>AD|RM)(?P<key>\w*)`),
-	INF: regexp.MustCompile(`(?P<key>\w{2})(?P<value>[\w\\s]*)`),
-}
-
-var inf = map[string]string{
-	"ID": "SXX4RUEEB263P3EX7VAGSMHGO4XVDBTQOJZNONI",
-	"PD": "4MH2IBPDTOP34ELXWSXRY35CSTHDR3PCOMWZPMI",
-	"CT": "1",
-	"NI": "nusbot",
-	"VE": escape("nusbot 0.2.0"),
-	"DE": escape("I'm a bot, type $help into the chat"),
-	"SU": "TCP4",
-}
-
-// helpers
+// Escapes a text according ADC.
 func escape(text string) string {
 	return strings.Replace(text, " ", `\s`, -1)
 }
 
+// Unescapes a text according ADC.
 func unescape(text string) string {
 	return strings.Replace(text, `\s`, " ", -1)
 }
 
+// Parses the string str with regex re and returns a map with the named group name as key and
+// the matched value as value.
 func parseArgs(re *regexp.Regexp, str string) map[string]string {
 	submatchMap := make(map[string]string)
 	submatches := re.FindStringSubmatch(str)
@@ -94,6 +106,8 @@ func parseArgs(re *regexp.Regexp, str string) map[string]string {
 	return submatchMap
 }
 
+// Parses a given string using the given regexp with named groups "key" and "value". A map is returned
+// with the value of named group "key" as key and value of "value" as value :)
 func parseKeyValues(re *regexp.Regexp, str string) map[string]string {
 	keyvalues := make(map[string]string)
 
@@ -114,6 +128,7 @@ func parseKeyValues(re *regexp.Regexp, str string) map[string]string {
 	return keyvalues
 }
 
+// Data structure with all information about the hub or client ADC connection.
 type AdcConnection struct {
 	target string
 	conn   net.Conn
@@ -122,6 +137,8 @@ type AdcConnection struct {
 	sid    string
 }
 
+// Entrypoint that starts a connection to the target and it's running the event loop.
+// Parses each incoming command and calls Handle() with parsed data.
 func (ac *AdcConnection) Run() {
 	conn, err := net.Dial("tcp", ac.target)
 	if err != nil {
@@ -150,6 +167,7 @@ func (ac *AdcConnection) Run() {
 	}
 }
 
+// Here's the place for the logic.
 func (ac *AdcConnection) Handle(msgType string, cmd string, args map[string]string, keyvalues map[string]string) {
 	switch cmd {
 	case STA:
