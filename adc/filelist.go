c9bb71a21e72ba8f8c900b4219e83d8bb180cb41
kraiz <lars.kreisz@gmail.com>
2014-11-18 22:22:50 +0100

    fixed xml parsing, startet diffing filelists

--- adc/filelist.go
+++ adc/filelist.go
@@ -1,55 +1,68 @@
 package adc
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"time"
+)
+
+type FileListing struct {
+	XMLName     xml.Name    `xml:"FileListing"`
+	Version     int         `xml:"Version,attr"`
+	CID         string      `xml:"CID,attr"`
+	Base        string      `xml:"Base,attr"`
+	Generator   string      `xml:"Generator,attr"`
+	Directories []Directory `xml:"Directory"`
+}
+
+type Directory struct {
+	XMLName     xml.Name    `xml:"Directory"`
+	Name        string      `xml:"Name,attr"`
+	Directories []Directory `xml:"Directory"`
+	Files       []File      `xml:"File"`
+}
+
+type File struct {
+	XMLName xml.Name `xml:"File"`
+	Name    string   `xml:"Name,attr"`
+	TTH     string   `xml:"TTH,attr"`
+	Size    uint64   `xml:"Size,attr"`
+}
+
+type Changelist struct {
+	CID       string
+	Nickname  string
+	FirstSeen string
+	LastScan  int64
+	Changes   []Change
+}
+
+type Change struct {
+	Time    time.Time
+	Added   []string
+	Removed []string
+}
+
+func (dir *Directory) CalcSize() uint64 {
+	var size uint64 = 0
+	for _, subDirectory := range dir.Directories {
+		size += subDirectory.CalcSize()
+	}
+	for _, subFile := range dir.Files {
+		size += subFile.Size
+	}
+	return size
+}
+
+func (dir *Directory) Diff(dir2 *Directory) ([]string, []string) {
+	// TODO
+}
+
+func (fl *FileListing) Diff(fl2 *FileListing) *Change {
+	// TODO
+}
 
 func ParseFilelist(data []byte) (*FileListing, error) {
 	var filelist FileListing
 	err := xml.Unmarshal(data, &filelist)
 	return &filelist, err
-}
-
-type FileListing struct {
-	XMLName   xml.Name `xml:"FileListing"`
-	Version   int      `xml:"Version,attr"`
-	CID       string   `xml:"CID,attr"`
-	Base      string   `xml:"Base,attr"`
-	Generator string   `xml:"Generator,attr"`
-	Children  []Node   `xml:"Directory"`
-}
-
-type Node interface {
-	Name() strin""g
-	Size() int64
-}
-
-type File struct {
-	XMLName xml.Name `xml:"File"`
-	name    string
-	size    int64
-	tth     string
-}
-
-type Directory struct {
-	XMLName  xml.Name `xml:"Directory"`
-	name     string   `xml:"Name,attr"`
-	children []Node
-}
-
-func (f *File) Name() string {
-	return f.name
-}
-
-func (f *File) Size() int64 {
-	return f.size
-}
-func (f *Directory) Name() string {
-	return f.name
-}
-
-func (f *Directory) Size() int64 {
-	var size int64 = 0
-	for _, childNode := range f.children {
-		size += childNode.Size()
-	}
-	return size
 }
--- changelist.go
+++ changelist.go
@@ -1,16 +1,1 @@
 package nusbot
-
-import "time"
-
-type Changelist struct {
-	Id        string
-	FirstSeen string
-	LastScan  int64
-	Changes   []Change
-}
-
-type Change struct {
-	Time    time.Time
-	Added   []string
-	Removed []string
-}
